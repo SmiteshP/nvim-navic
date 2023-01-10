87bc5699afece7996ad99968ed9417f5b98bd0f1
nyngwang <nyngwang@gmail.com>
2023-01-10 10:11:10 +0800

    should use tabs instead of 4 spaces.

--- lua/nvim-navic/init.lua
+++ lua/nvim-navic/init.lua
@@ -498,48 +498,49 @@ function M.get_location(opts)
 		end
 	end
 
-  if type(local_config.depth_limit) == "function" then
-    local context = {
-      winid = vim.api.nvim_get_current_win(),
-      path = vim.fn.expand('%:p'),
-      mod = vim.bo.mod,
-    }
-    if type(local_config.depth_limit(context)) == "number" then
-      local start_index = 0
-      while
-        table.concat(
-          vim.list_slice(location, start_index + 1, #location),
-          (local_config.highlight and "%#NavicSeparator#" or '')
-          .. local_config.separator
-          .. (local_config.highlight and "%*" or ""))
-        :len() > local_config.depth_limit(context) do
-        start_index = start_index + 1
-      end
-      location = vim.list_slice(location, start_index, #location)
-      if start_index > 1 then
-        if local_config.highlight then
-          table.insert(location, 1, "%#NavicSeparator#" .. local_config.depth_limit_indicator .. "%*")
-        else
-          table.insert(location, 1, local_config.depth_limit_indicator)
-        end
-      end
-    else
-      local_config.depth_limit = 0
-    end
-  end
+	if type(local_config.depth_limit) == "function" then
+		local context = {
+			winid = vim.api.nvim_get_current_win(),
+			path = vim.fn.expand('%:p'),
+			mod = vim.bo.mod,
+		}
+		if type(local_config.depth_limit(context)) == "number" then
+			local start_index = 0
+			while
+				table.concat(
+				vim.list_slice(location, start_index + 1, #location),
+					(local_config.highlight and "%#NavicSeparator#" or '')
+					.. local_config.separator
+					.. (local_config.highlight and "%*" or ""))
+				:len() > local_config.depth_limit(context)
+			do
+				start_index = start_index + 1
+			end
+			location = vim.list_slice(location, start_index, #location)
+			if start_index > 1 then
+				if local_config.highlight then
+					table.insert(location, 1, "%#NavicSeparator#" .. local_config.depth_limit_indicator .. "%*")
+				else
+					table.insert(location, 1, local_config.depth_limit_indicator)
+				end
+			end
+		else
+			local_config.depth_limit = 0
+		end
+	end
 
-  if type(local_config.depth_limit) == "number"
-    and local_config.depth_limit ~= 0 and #location > local_config.depth_limit then
-    local start_index = #location - local_config.depth_limit + 1
+	if type(local_config.depth_limit) == "number"
+		and local_config.depth_limit ~= 0 and #location > local_config.depth_limit then
+		local start_index = #location - local_config.depth_limit + 1
 		location = vim.list_slice(location, start_index, #location)
-    if start_index > 1 then
-      if local_config.highlight then
-        table.insert(location, 1, "%#NavicSeparator#" .. local_config.depth_limit_indicator .. "%*")
-      else
-        table.insert(location, 1, local_config.depth_limit_indicator)
-      end
-    end
-  end
+		if start_index > 1 then
+			if local_config.highlight then
+				table.insert(location, 1, "%#NavicSeparator#" .. local_config.depth_limit_indicator .. "%*")
+			else
+				table.insert(location, 1, local_config.depth_limit_indicator)
+			end
+		end
+	end
 
 	local ret = ""
 
